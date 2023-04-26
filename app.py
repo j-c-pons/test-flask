cd30da12b7c6693fe117b2d29f9456788ee45226
jean-christophe <jean-christophe.pons@epitech.eu>
2023-04-26 16:28:46 +0200

    dernier commit

--- app.py
+++ app.py
@@ -1,22 +1,60 @@
-from flask import Flask
-import mysql.connector
+import os
+from flask import Flask, request, json
+from flask_sqlalchemy import SQLAlchemy, asc
+import datetime
 
-
-class Personne:
-    def __init__(self, nom, prenom, date_naissance):
-        self.nom = nom
-        self.prenom = prenom
-        self.date_naissance = date_naissance
-
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
-'''
-@app.route("/")
-def hello():
-    return "hello"
-'''
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/newPerson', methods=['POST'])
-def create_personne():
+db = SQLAlchemy(app)
+
+class Personne(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nom = db.Column(db.String(100))
+    prenom = db.Column(db.String(100))
+    date_naisse = db.Column(db.Date)        
+
+app = Flask(__name__)
+
+@app.route('/create/', methods=('POST'))
+def create():
+    _nom = request.form['nom']
+    _prenom = request.form['prenom']
+    _date_naissance = request.form['date_naissance']
+
+    if getAge(_date_naissance) >=150:
+        return "L'âge fourni pour "+_prenom+" "+_nom+" est trop élevé: "+_date_naissance, 400
+    else:
+        newPerson = Personne(nom=_nom,
+                            prenom=_prenom,
+                            date_naissance=_date_naissance)
+        db.session.add(newPerson)
+        db.session.commit()
+        return "Nouvelle personne ajoutée: "+_nom+" "+_prenom, 201
+
+
+@app.route('/display_all/', methodes=('GET'))
+def display_all():
+    personnes = Personne.query.all().order_by(asc(Personne.nom))
+    personnes_list=[]
+    
+    for personne in personnes:
+        personnes_list.append({"nom":personne.nom, "prenom":personne.prenom, "date_naissance":personne.date_naissance, "age":getAge(personne.date_naissance)})
+    
+    response = app.response_class(
+        response=json.dumps(personnes_list),
+        status=200,
+    )
+    return response
+
+
+def getAge(date):
+        today = datetime.date.today()
+        difference_dates = today - date
+        age = difference_dates.days // 365
+        return age
--- table.py
+++ table.py
@@ -1,22 +1,3 @@
-import mysql.connector
-
-conn = mysql.connector.connect(
-    host="localhost", 
-    user="user",
-    password="password",  
-    database="dbname"
-)
-
-cursor = conn.cursor()
-cursor.execute("""
-    CREATE TABLE Personne (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        nom VARCHAR(50) NOT NULL,
-        prenom VARCHAR(50) NOT NULL,
-        date_naissance DATE NOT NULL
-    )
-""")
-               
-conn.commit()
-cursor.close()
-conn.close()
+from app import db
+db.drop_all()
+db.create_all()
